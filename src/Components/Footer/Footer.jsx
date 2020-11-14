54cf56972dd97f5a6756e2c70e028c7cf546d57f
Arthur Guez <arthurguez29@gmail.com>
2020-11-14 17:12:24 +0100

    added footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="footer">
+			<div className="footer__line"></div>
+			<ul className="footer__links">
+				<li>About</li>
+				<li>FAQ</li>
+				<li>Contact</li>
+				<li>Terms Of Service</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Footer;
